6d41d07d54682fe9778cc0f11f3907d01531c3aa
chandantm317 <chandantmchandu31@gmail.com>
2023-12-06 10:12:35 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/*
+ * Header
+ *   - Logo
+ *   - Nav Items
+ * Body
+ *   - Search
+ *   - RestaurantContainer
+ *      - RestaurantCard
+ * Footer
+ *   - Copyright
+ *   - Links
+ *   - Address
+ *   - Contact
+ */
+const AppLayout = () => {
+  return <div className="app"> </div>;
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
