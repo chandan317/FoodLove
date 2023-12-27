c502a78bd6abc4beb98c68115cb8a84ced2afed6
chandantm317 <chandantmchandu31@gmail.com>
2023-12-27 09:42:38 +0530

    props commited

--- App.js
+++ App.js
@@ -41,15 +41,18 @@ const Header = () => {
   );
 };
 // const color = { backgroundColor: "yellow" };
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+  {
+    console.log(props);
+  }
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
       <img
         className="res-logo"
         src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/unionl8yysinb1g9zm49"
       />
-      <h3>Meghana Foods</h3>
-      <h4>Dosa , South Indian </h4>
+      <h3>{props.resName}</h3>
+      <h4>{props.cuisine} </h4>
       <h4>4.4 stars</h4>
       <h4>38 minutes</h4>
     </div>
@@ -60,7 +63,11 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard />
+        <RestaurantCard
+          resName="Megahana foods"
+          cuisine="andhra food , south indian"
+        />
+        <RestaurantCard resName="kfc " cuisine="american food , burger" />
       </div>
     </div>
   );
